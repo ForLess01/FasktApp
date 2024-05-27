a3a2c49afbc99d4d79d81858b12a20e6765672cd
RendoAlf <studentralfotar@gmail.com>
2024-05-27 01:56:51 -0500

    Started, fragment_time__line__week

--- app/src/main/java/com/alfontetarqui/fasktapp/TimeLine_Main_Activity.kt
+++ app/src/main/java/com/alfontetarqui/fasktapp/TimeLine_Main_Activity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.alfontetarqui.fasktapp.databinding.ActivityTimeLineMainBinding
 
 class TimeLine_Main_Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityTimeLineMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_time_line_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityTimeLineMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Configurar escuchador de eventos para la barra de navegación
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_calendar -> {
+                    replaceFragment(())
+                    true
+                }
+
+                }
+                else -> false
+            }
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.TimeLine_Fragment_Container, fragment)
+            .commit()
     }
 }
