1f25a4bf031997571779701c10bdd879ff2d5846
RendoAlf <studentralfotar@gmail.com>
2024-05-30 23:09:42 -0500

    adding fragment_notes.xml & fragment_notes_main.xml , It is a part of main sections.

--- app/src/main/java/com/alfontetarqui/fasktapp/NotesMainFragment.kt
+++ app/src/main/java/com/alfontetarqui/fasktapp/NotesMainFragment.kt
@@ -5,55 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.commit
+import com.alfontetarqui.fasktapp.databinding.FragmentNotesMainBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NotesMainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NotesMainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentNotesMainBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes_main, container, false)
+        _binding = FragmentNotesMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NotesMainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NotesMainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.VisibleMainTaskView1.setOnClickListener { launchNewFragment() }
+    }
+
+    private fun launchNewFragment() {
+        val newFragment = NotesFragment() // Crear una instancia del nuevo fragmento
+
+        // Realizar la transacción del fragmento
+        parentFragmentManager.commit {
+            replace(R.id.nav_host_fragment_container, newFragment) // Asegúrate de que fragment_container_view es el ID correcto del contenedor del fragmento
+            addToBackStack(null) // Opcional: agrega esta transacción a la pila hacia atrás para que el usuario pueda volver con el botón de retroceso
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
