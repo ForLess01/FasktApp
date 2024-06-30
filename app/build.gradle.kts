f9be3688c9b0544931fc4ee2045bac1841e18deb
RendoAlf <studentralfotar@gmail.com>
2024-06-30 12:34:12 -0500

    Se Agrego SDK firebase, y deteccion de Internet en el activity main : ONLINE u OFFLINE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -40,7 +41,8 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation("com.google.firebase:firebase-analytics")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/alfontetarqui/fasktapp/MainActivity.kt
+++ app/src/main/java/com/alfontetarqui/fasktapp/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.alfontetarqui.fasktapp
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -27,5 +30,20 @@ class MainActivity : AppCompatActivity() {
             // Finalizar la ActivityA (opcional)
             finish()
         }
+
+        checkInternetConnection()
     }
+    private fun checkInternetConnection() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val network = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(network)
+        val isConnected = networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) == true
+
+        if (isConnected) {
+            binding.TextOnlineOFF.text = "ONLINE"
+        } else {
+            binding.TextOnlineOFF.text = "OFFLINE"
+        }
+    }
+
 }
